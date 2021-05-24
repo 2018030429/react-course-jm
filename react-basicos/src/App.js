7c539c5e2f5edb999c2383f206e313c8ddfde104
Enrique Contreras <2018030429@upsin.edu.mx>
2021-05-24 17:04:20 -0600

    Hooks - useEffect (1/2)

--- react-basicos/src/App.js
+++ react-basicos/src/App.js
@@ -11,6 +11,7 @@ import Padre from "./components/ComunicacionComponentes";
 import CicloVida from "./components/CicloVida";
 import AjaxApis from "./components/AjaxApis";
 import ContadorHooks from "./components/ContadorHooks";
+import ScrollHooks from "./components/ScrollHooks";
 
 function App() {
   let name = "Henry";
@@ -75,6 +76,8 @@ function App() {
           <AjaxApis/>
           <hr/>
           <ContadorHooks title="seguidores"/>
+          <hr/>
+          <ScrollHooks/>
         </section>
       </header>
     </div>
---
+++ react-basicos/src/components/ScrollHooks.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, Fragment } from "react";
+
+export default function ScrollHooks() {
+
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    console.log("Moviendo el scroll");
+    const detectarScroll = () => setScrollY(window.pageYOffset);
+    window.addEventListener('scroll', detectarScroll);
+  }, [scrollY]);
+
+  useEffect(() => {
+    console.log("Fase de Montaje");
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      console.log("Fase de Desmontaje");
+    }
+  });
+
+  return (
+    <Fragment>
+      <h2> Hooks - useEffect & lifecycle</h2>
+      <p> ScrollY del Navegador { scrollY } px </p>
+    </Fragment>
+  );
+}
