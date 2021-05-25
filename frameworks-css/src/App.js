59aa06ea17b5a68dd3d93a3fd2a41fcdbd3882b4
Enrique Contreras <2018030429@upsin.edu.mx>
2021-05-25 16:05:21 -0600

    Bootstrap & Bulma

--- frameworks-css/src/App.js
+++ frameworks-css/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// * Components
+// import Bootstrap from "./Components/Bootstrap";
+import Bulma from "./Components/Bulma";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> Frameworks CSS with React </h2>
+      {/* <Bootstrap/> */}
+      <Bulma/>
     </div>
   );
 }
