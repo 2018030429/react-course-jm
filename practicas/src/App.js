cadd71c5d63f14a108a3475c670c1a65a100b26e
Enrique Contreras <2018030429@upsin.edu.mx>
2021-06-01 14:57:09 -0600

    selects (1/3)

--- practicas/src/App.js
+++ practicas/src/App.js
@@ -2,12 +2,15 @@ import React, { Fragment } from "react";
 
 import CRUDAPP from "./components/CRUD";
 import CrudApi from "./components/CrudApi";
+import SelectsAnidados from "./components/SelectsAnidados";
 import SongSearch  from "./components/SongSearch";
 
 function App() {
   return (
     <Fragment>
       <h1> React Excercises </h1>
+      <SelectsAnidados/>
+      <hr/>
       <SongSearch/>
       <hr/>
       <CrudApi/>
---
+++ practicas/src/components/SelectList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SelectList = () => {
+  return (
+    <div>
+      <select>
+        <option></option>
+      </select>
+    </div>
+  )
+}
+
+export default SelectList
