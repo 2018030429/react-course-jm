1a8af407ae697e6b3020baae2e8e8752f28e7e3a
Enrique Contreras <2018030429@upsin.edu.mx>
2021-06-01 14:25:12 -0600

    searchsong details

--- practicas/src/components/SongArtist.js
+++ practicas/src/components/SongArtist.js
@@ -5,7 +5,7 @@ const SongArtist = ({ artist }) => {
     <section>
       <h3> { artist.strArtist } </h3>
       <img src={ artist.strArtistThumb } alt={ artist.strArtist } />
-      <p> { artist.intBornYear || artist.intFormedYear } - { artist.intDiedYear || 'Present' } </p>
+      <p> { artist.intBornYear || artist.intFormedYear } - { artist.intDiedYear || artist.strDisbanded? 'Disbanded' : 'Present' } </p>
       <p> { artist.strCountry } - { artist.strCountryCode } </p>
       <p> { artist.strGenre } -  { artist.strStyle } </p>
       <a href={ `https://${ artist.strWebsite }` } target="_blank" rel="noreferrer"> Official Website </a>
