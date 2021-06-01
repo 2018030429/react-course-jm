a18b432d8790e76acaebde9ce253ac180ae340d9
Enrique Contreras <2018030429@upsin.edu.mx>
2021-05-31 18:18:50 -0600

    searchsong (3/5) & (4/5)

--- practicas/src/components/SongDetails.js
+++ practicas/src/components/SongDetails.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Message from './Message';
 
 import SongArtist from "./SongArtist";
 import SongLyrics from './SongLyrics';
 
 const SongDetails = ({ search, lyric, bio }) => {
+
+  if ([lyric, bio].includes(null)) return null;
+
   return (
-    <div>
-      <h2> Details </h2>
-      <SongArtist/>
-      <SongLyrics/>
-    </div>
+    <Fragment>
+      { lyric.error || lyric.err || lyric.name === "AbortError" ? (
+        <Message 
+          message={`Error: The ${search.song} song doesn't exist`} 
+          bgColor="#DC3545"/>
+      ):(
+        <SongLyrics/> 
+      )}
+      { bio.artists ? (
+        <SongArtist/>
+      ):(
+        <Message 
+          message={`Error: The artist ${ search.artist } doesn't exist`} 
+          bgColor="#DC3545"/>
+      )}
+    </Fragment>
   )
 }
 
--- practicas/src/components/SongSearch.js
+++ practicas/src/components/SongSearch.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Loader from './Loader';
 import SongDetails from './SongDetails';
 import SongForm from './SongForm';
+import { helpHttp } from "../helpers/help.http";
 
 const SongSearch = () => {
 
@@ -11,8 +12,33 @@ const SongSearch = () => {
   const [biography, setBiography] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    if (search === null) return;
+
+    (async () => {
+      const { artist, song } = search;
+
+      let artistURL = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${ artist }`;
+      let songURL = `https://api.lyrics.ovh/v1/${ artist }/${ song }`;
+
+      setLoading(true);
+
+      const [artistRes, songRes] = await Promise.all([
+        helpHttp().get(artistURL), 
+        helpHttp().get(songURL)
+      ]);
+
+      setLoading(false);
+
+      setBiography(artistRes);
+      setLyric(songRes);
+
+    })();
+
+  }, [search]);
+
   const handleSearch = (data) => {
-    setSearch(data)
+    setSearch(data);
   }
 
   return (
@@ -20,7 +46,9 @@ const SongSearch = () => {
       <h2> SongSearch </h2>
       { loading && <Loader/> }
       <SongForm handleSearch={ handleSearch }/>
-      <SongDetails search={ search } lyric={ lyric } bio={ biography }/>
+      { search && !loading && (
+        <SongDetails search={ search } lyric={ lyric } bio={ biography }/>
+      )}
     </div>
   )
 }
