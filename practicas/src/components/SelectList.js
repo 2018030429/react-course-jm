2f245b85ed4586cde976de1465a6e3f4bca89edc
Enrique Contreras <2018030429@upsin.edu.mx>
2021-06-01 16:34:18 -0600

    selects (3/3)

--- practicas/src/components/SelectList.js
+++ practicas/src/components/SelectList.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const SelectList = () => {
+import { useFetch } from "../hooks/useFetch";
+
+import Loader from "./Loader";
+import Message from "./Message";
+
+const mapTranslator = {
+  states: "estado",
+  towns: "municipios",
+  suburbs: "colonia"
+}
+
+const SelectList = ({ title, url, handleChange }) => {
+
+  const { data, error, loading } = useFetch(url);
+
+  if (!data) return null;
+  if (error) {
+    return <Message 
+      message={`Error ${ error.status }: ${ error.statusText }`} 
+      bgColor="#DC3545"/>
+  }
+
+  let id = `select-${ title }`;
+  let label = title.charAt(0).toUpperCase() + title.slice(1);
+  let options = data.response[mapTranslator[title]];
+
   return (
-    <div>
-      <select>
-        <option></option>
+    <Fragment>
+      <label htmlFor={id}>{ label }</label>
+      { loading && <Loader/> }
+      <select name={id} id={id} onChange={ handleChange }>
+        <option> Select { title.slice(0,-1) } </option>
+        { data && (
+          options.map(item => <option key={item} value={item}>{ item }</option>)
+        )}
       </select>
-    </div>
+    </Fragment>
   )
 }
 
--- practicas/src/components/SelectsAnidados.js
+++ practicas/src/components/SelectsAnidados.js
@@ -8,27 +8,29 @@ const SelectsAnidados = () => {
   const [town, setTown] = useState("");
   const [suburb, setSuburb] = useState("");
 
+  const TOKEN = "a8d4c2c4-cd9c-4ea8-aa15-94591ae7a7e8";
+
   return (
     <div>
       <h2> Selects Anidados </h2>
       <h3> México </h3>
-      
+
       <SelectList 
         title="states" 
-        url="" 
+        url={`https://api.copomex.com/query/get_estados?token=${TOKEN}`}   
         handleChange={(e) => { setState(e.target.value) }} />
 
       { state && (
         <SelectList 
           title="towns" 
-          url="" 
+          url={`https://api.copomex.com/query/get_municipio_por_estado/${state}?token=${TOKEN}`}
           handleChange={(e) => {setTown(e.target.value)}} />
       )}
 
       { town && (
         <SelectList 
           title="suburbs" 
-          url="" 
+          url={`https://api.copomex.com/query/get_colonia_por_municipio/${town}?token=${TOKEN}`} 
           handleChange={(e) => {setSuburb(e.target.value)}} />
       )}
       
